4df3c3231ca3b8b73feaab474929ebec6c751b9a
ToniBuc <59138588+ToniBuc@users.noreply.github.com>
2021-07-09 23:22:49 +0200

    Added solutions for 2 more problems

---
+++ src/main/java/com/github/tonibuc/ReverseString.java
@@ -0,0 +1,19 @@
+package main.java.com.github.tonibuc;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String x, y = "";
+        System.out.println("Enter a string you want reversed:");
+        x = scanner.next();
+
+        int l = x.length();
+        for (int i = x.length()-1; i >= 0; i-- ){
+            y += x.charAt(i);
+        }
+
+        System.out.println(y);
+    }
+}
